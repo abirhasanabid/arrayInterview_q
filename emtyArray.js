79dca2bb7f6d34fdd410a8342a1f73c71ce28960
Abid Hasan Abir <abid.7.tm@gmial.com>
2024-08-31 21:16:09 +0600

    added emty array

--- emtyArray.js
+++ emtyArray.js
@@ -1,2 +1,5 @@
 // Question 1: How do you create an empty array in JavaScript?
 
+const arr = [];  //using literal syntax
+
+const array = new Array(); //using the constructor 
---
+++ first&lastElementArray.js
@@ -0,0 +1,4 @@
+// Question 2: How do you access the first and last elements of an array?
+
+const num = [1,3,67,7,45];
+console.log(num[0]);
